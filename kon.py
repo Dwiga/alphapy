89f8458a7aee522f30058fc35bb7789cd9da0908
dwiga <dwi.hyuga@gmail.com>
2017-04-20 21:59:47 +0700

    + connect to mysql

--- kon.py
+++ kon.py
@@ -1,4 +1,9 @@
 import sqlite3
+import pymysql
 
-con = sqlite3.connect("alpha.db")
-exe = con.cursor()
+def mylyt():
+    con = sqlite3.connect("alpha.db")
+    exe = con.cursor()
+def oursql():
+    db = pymysql.connect(host="localhost", user="root", passwd="itmatrix", db="matrix_db")
+    exe = db.cursor()
