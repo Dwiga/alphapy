fdffeee5e8308be27dbe1994d25993f6632988e8
dwiga <dwi.hyuga@gmail.com>
2017-04-21 20:26:25 +0700

    mysql connected, data not saved

--- act.py
+++ act.py
@@ -1,7 +1,10 @@
-from kon import mylyt
+from kon import db, kusi
 import datetime
+db = db
+kusi = kusi
 
 tday = datetime.date.today()
 
-def cba(isi):
-    print (isi)
+def cba(isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8):
+    kusi.execute("insert into movie (title, tanggal, episode, keterangan, rating, country, lang, genre, tglinput) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8, tday))
+    db.commit()
