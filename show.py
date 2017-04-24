05e3defb977cb3e518c1fca33b8898a7b3545207
dwiga <dwi.hyuga@gmail.com>
2017-04-24 19:46:22 +0700

    show data succeed

--- show.py
+++ show.py
@@ -1,19 +1,24 @@
 import tkinter
 from tkinter import *
-from kon import *
+from kon import con, exe
 from tkinter import ttk
 
 daeh = tkinter.Tk()
+daeh.geometry("320x240")
 tree = ttk.Treeview(daeh)
 
-tree['columns']=("one", "two")
+tree["columns"] = ("one", "two", "three")
 tree.column("one", width=100)
 tree.column("two", width=100)
-tree.heading("one", text="Column A")
-tree.heading("two", text="column B")
+tree.column("three", width=100)
 
-a = exe.execute("select * from movie")
+tree.heading("one", text="NAME")
+tree.heading("two", text="VOTES")
+tree.heading("three", text="PERSENTAGE")
+
+a = exe.execute("""select * from movie""")
 for wor in a:
     tree.insert('', 'end', values=(wor[1], wor[2], wor[3]))
 
+tree.pack()
 mainloop()
