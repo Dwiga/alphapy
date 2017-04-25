8fff7e0ce0a84d92e86db9616435ac3c7266d0fd
dwiga <dwi.hyuga@gmail.com>
2017-04-25 21:48:10 +0700

    didn't change anything

--- act.py
+++ act.py
@@ -8,3 +8,4 @@ tday = datetime.date.today()
 def cba(isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8):
     kusi.execute("insert into movie (title, tanggal, episode, keterangan, rating, country, lang, genre, tglinput) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8, tday))
     db.commit()
+    
