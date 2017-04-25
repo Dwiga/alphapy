7b30ac8572f5aad5f8ea413d1ccca1e1a69ad503
dwiga <dwi.hyuga@gmail.com>
2017-04-25 21:47:24 +0700

    try to add button action every data

--- show.py
+++ show.py
@@ -1,24 +1,28 @@
 import tkinter
 from tkinter import *
 from kon import con, exe
+from alpha import tst
 from tkinter import ttk
 
 daeh = tkinter.Tk()
 daeh.geometry("320x240")
 tree = ttk.Treeview(daeh)
 
-tree["columns"] = ("one", "two", "three")
+tree["columns"] = ("one", "two", "three", "four")
 tree.column("one", width=100)
 tree.column("two", width=100)
 tree.column("three", width=100)
+tree.column("four", width=100)
 
 tree.heading("one", text="NAME")
 tree.heading("two", text="VOTES")
 tree.heading("three", text="PERSENTAGE")
+tree.heading("four", text="button")
 
 a = exe.execute("""select * from movie""")
 for wor in a:
-    tree.insert('', 'end', values=(wor[1], wor[2], wor[3]))
+    b = Button(daeh, text="Hapus", command = lambda : tst(wor[0])).grid(row=9, column=2, sticky=W, pady=4)
+    tree.insert('', 'end', values=(wor[1], wor[2], wor[3], b))
 
-tree.pack()
+"""tree.pack()"""
 mainloop()
