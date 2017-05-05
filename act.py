16c816d6db5c67211df81d8f015daf41fb2faa25
dwiga <dwi.hyuga@gmail.com>
2017-05-05 20:57:22 +0700

    trying to add button on treeview

--- act.py
+++ act.py
@@ -9,3 +9,6 @@ def cba(isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8):
     kusi.execute("insert into movie (title, tanggal, episode, keterangan, rating, country, lang, genre, tglinput) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (isi1, isi2, isi3, isi4, isi5, isi6, isi7, isi8, tday))
     db.commit()
     
+def btn(dat1):
+    print(dat1)
+    
--- show.py
+++ show.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import *
 from kon import con, exe
+from act import btn
 from alpha import tst
 from tkinter import ttk
 
@@ -8,21 +9,23 @@ daeh = tkinter.Tk()
 daeh.geometry("320x240")
 tree = ttk.Treeview(daeh)
 
-tree["columns"] = ("one", "two", "three", "four")
+tree["columns"] = ("one", "two", "three")
 tree.column("one", width=100)
 tree.column("two", width=100)
 tree.column("three", width=100)
-tree.column("four", width=100)
+"""tree.column("four", width=100)"""
 
 tree.heading("one", text="NAME")
 tree.heading("two", text="VOTES")
 tree.heading("three", text="PERSENTAGE")
-tree.heading("four", text="button")
+"""tree.heading("four", text="button")"""
 
 a = exe.execute("""select * from movie""")
 for wor in a:
-    b = Button(daeh, text="Hapus", command = lambda : tst(wor[0])).grid(row=9, column=2, sticky=W, pady=4)
-    tree.insert('', 'end', values=(wor[1], wor[2], wor[3], b))
+    """b = Button(daeh, text="Hapus", command = lambda : tst(wor[0])).grid(row=9, column=2, sticky=W, pady=4)"""
+    tree.insert('', 'end', values=(wor[1], wor[2], wor[3]))
 
-"""tree.pack()"""
+tree.bind("<button-1>", btn(wor[1]))
+
+tree.pack()
 mainloop()
